46ef648f60a36c2b1d3fc0ebcd968ed9b210125f
ssong <98401648+hardjava@users.noreply.github.com>
2025-05-22 01:57:20 +0900

    schedule db 생성 (#1)
    - schedule.sql 에 DDL 작성하여 schedule db 생성

---
+++ schedule.sql
@@ -0,0 +1,13 @@
+use `schedule-app`;
+
+
+-- create table (Lv.0 - 2)
+CREATE TABLE schedule
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'schedule identifier',
+    username VARCHAR(50) NOT NULL COMMENT 'author name',
+    contents TEXT NOT NULL COMMENT 'to do content',
+    password VARCHAR(50) NOT NULL COMMENT 'password',
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'date created',
+    modified_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'date modified'
+);
